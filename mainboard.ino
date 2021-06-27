8005229a434c3ad8d41cb84f314690aa94b8b6ba
FurretGoWalcc <73409226+FurretGoWalcc@users.noreply.github.com>
2021-06-26 22:30:11 -0500

    Simplified mainboard
    Many features went to the corporate cutting floor in this commit- imu data, buzzers, water sensors. Things that will undoubtedly be needed eventually.

--- mainboard.ino
+++ mainboard.ino
@@ -1,25 +1,11 @@
 // Teensy++ 2.0
 
-#include <LSM9DS1_Registers.h>
-#include <LSM9DS1_Types.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <SparkFunLSM9DS1.h>
 #include <Servo.h>
-
-#define LSM9DS1_M 0x1E   // Would be 0x1C if SDO_M is LOW
-#define LSM9DS1_AG 0x6B  // Would be 0x6A if SDO_AG is LOW
 
 #define SerialConnection Serial
 
-LSM9DS1 imu;
-/*
-const int buzzer = 17;
-const int water1 = 20;
-const int water2 = 21;
-const int water3 = 22;
-const int water4 = 23;
-*/
 const int COMMAND_SIZE = 52;
 const int MOTOR_NEUTRAL = 1500;
 
@@ -29,38 +15,17 @@ String disabledCommand = ":1500;1500;1500;1500;1500;1500;1500;1500;1500;1500;0";
 
 void setup()
 {
-  
   delay(6000); 
 
   Wire.begin();
   SerialConnection.begin(115200);
   SerialConnection.setTimeout(80); //80?
   
-  imu.settings.device.commInterface = IMU_MODE_I2C;
-  imu.settings.device.mAddress = LSM9DS1_M;
-  imu.settings.device.agAddress = LSM9DS1_AG;
-  
-  // pinMode(buzzer, OUTPUT);
-  // pinMode(water4, INPUT);
-
-  //  Scream untill IMU connected
-  //  digitalWrite(buzzer, 1);
-
-  while (!imu.begin())
+  // This section is designed to give the i2c connection time to initialize (maybe unnecessary?)
+  while (timer < 50)
   {
-    delay(500);
-  }
-
-  //  digitalWrite(buzzer, 0);
-}
-
-void loop()
-{
-  //  This section 'initializes' the communication
-  static bool wireInit = false;
-  while (!wireInit)
-  {
-    for (int i = 11; i<14; i++) 
+    // Note: talk about i2c addresses 
+    for (int i = 10; i<13; i++) 
     {
       Wire.beginTransmission(i);
       Wire.write("1500");
@@ -69,39 +34,14 @@ void loop()
       Wire.endTransmission();
       delay(90);
       ++timer;
-    
-      if (timer > 50)
-      {
-        wireInit = true;
-      }
     }
   }
+}
+
+void loop()
+{
   char driveCommands[COMMAND_SIZE];
-
   ++timer;
-
-  float ax = 0.0, ay = 0.0, az = 0.0;
-
-  if (imu.accelAvailable())
-  {
-    // Updates ax, ay, and az
-    imu.readAccel();
-    ax = imu.ax;
-    ay = imu.ay;
-    az = imu.az;
-  }
-  /*
-  if (imu.gyroAvailable())
-  {
-    //  Updates gx, gy, and gz
-    imu.readGyro();
-  }
-  */
-  float roll = atan2(ay, az) * 180 / PI;
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az)) * 180 / PI;
-
-  char cstr[16];
-  itoa(timer, cstr, 10);
 
   // Wait untill there is at least 1 full command to read
   if (SerialConnection.available() >= COMMAND_SIZE - 1)
@@ -111,28 +51,15 @@ void loop()
     {
       timer = 0;  // Reset timer if valid data received
 
-      // Only send data back if data was received
-/*    if (digitalRead(water4) && digitalRead(water3) && digitalRead(water2) && digitalRead(water1))
-      {
-        writeString("0");
-      }
-      else
-      {
-        writeString("1");
-      }
-*/
       String info;
       info = disabledCommand;
-
       info = SerialConnection.readStringUntil('\n');
       info.remove(COMMAND_SIZE-1);
       info.toCharArray(driveCommands, COMMAND_SIZE - 1);
       drive(driveCommands);
-
+      
       writeString(info);
-
-      // digitalWrite(buzzer, 1);
-
+      
       clearSerial();
     }
     else
@@ -142,15 +69,14 @@ void loop()
     }
   }
   
-  // Only run if a command has been received within 25 ticks
+  // Set motors to neutral if no command has been received within 25 loop() cycles
   if (timer > 25)
   {
     disabledCommand.toCharArray(driveCommands, COMMAND_SIZE - 1); 
     drive(driveCommands);
   }
-  //Rough timer counting
+  //Rough timer counting (Ensure counter is at least one ms)
   delay(1);
-  // digitalWrite(buzzer, 0);
 }
 
 void clearSerial() {
@@ -163,10 +89,8 @@ void clearSerial() {
 // Used to serially push out a String with Serial.write()
 void writeString(String stringData)
 {
-  for (unsigned int i = 0; i < stringData.length(); i++)
-  {
-    SerialConnection.write(stringData[i]);  // Push each char 1 by 1 on each loop pass
-  }
+  for (char c : stringData)
+    SerialConnection.write(c);  // Push each char 1 by 1 on each loop pass
 }
 
 void drive(char array[])
@@ -181,7 +105,7 @@ void drive(char array[])
     index++;
     ptr = strtok(NULL, ";");
   }
-
+  
   Wire.beginTransmission(10);
   Wire.write(commands[0]);
   Wire.write(commands[3]);
