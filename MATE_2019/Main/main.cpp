3b2db24df809aa27e425143ccc96a218e949c95a
Cade Wilkerson <cadewilkerson@protonmail.com>
2022-01-29 18:40:40 -0600

    Update main.cpp

--- MATE_2019/Main/main.cpp
+++ MATE_2019/Main/main.cpp
@@ -6,19 +6,13 @@
 #include ".\Headers\Utils.h"
 
 
-
-
-
 /* CODE FOR TESTING ARDUINO Tx/Rx */
-
-
-
-
 
 
 using namespace std;
 
 char output[MAX_DATA_LENGTH]; // This is currently 255
+char out[MAX_DATA_LENGTH];
 
 const int sleepTime = 90; // Added a new variable to easily change the command sleep time
 
@@ -26,6 +20,24 @@ const int sleepTime = 90; // Added a new variable to easily change the command s
 // Must remember that the backslashes are essential so do not remove them
 const char* port = "\\\\.\\COM3";
 SerialPort arduino(port, 115200);
+
+char* transferData(string data)
+{
+  // Convert data string to character array
+  char* charArray = new char[data.size()];
+  copy(data.begin(), data.end(), charArray);
+  // Send command array to arduino
+  arduino.writeSerialPort(charArray, data.size() - 1);
+  delete[] charArray;
+
+  // Wait for arduino to process command
+  Sleep(sleepTime); // This value will need to be adjusted as we test the ROV (how long each command typically takes)
+
+  // Expects to receive the command string echoed back, unless there is a water leak, then it will just recieve 'W'
+  arduino.readSerialPort(output, MAX_DATA_LENGTH);
+  
+  return output;
+}
 
 // Takes different test strings to send/receive back from the Arduino
 int main()
@@ -46,28 +58,18 @@ int main()
     cout << "Input info: ";
     getline(cin, test_data);
     cout << " Sending: " << test_data << endl;
-
-    char* charArray = new char[test_data.size()];
-    copy(test_data.begin(), test_data.end(), charArray);
     
-    // Send command array to arduino
-    arduino.writeSerialPort(charArray, data.size() - 1);
-    delete[] charArray;
-
-    // Wait for arduino to process command
-    Sleep(sleepTime); // This value will need to be adjusted as we test the ROV (how long each command typically takes)
-
-    // Expects to receive the command string echoed back, unless there is a water leak, then it will just recieve 'W'
-    arduino.readSerialPort(output, MAX_DATA_LENGTH);
+    out = transferData(test_data);
 
     // The loop will output each of the characters in the output array
     cout << " Received: ";
-    for (char c : output)
+    for (char c : out)
     {
       cout << c;
     }
     cout << endl;
   }
+  
   cout << " Exiting" << endl;
 
   return 0;
